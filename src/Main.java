21446ea9f38c16fa66c716f8e2d937cd7958648a
Your Name <you@example.com>
2023-02-15 23:47:55 +1200

    fix monthPercent formula

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ public class Main {
 
         System.out.println();
         System.out.println("Кредит на 1 год. Ежемесячный платёж:");
-        System.out.println(service.calculate (1000000, 12, 9.99) + " рублей");
+        System.out.println(service.calculate(1000000, 12, 9.99) + " рублей");
 
         System.out.println();
         System.out.println("Кредит на 2 года. Ежемесячный платёж:");
-        System.out.println(service.calculate (1000000, 24, 9.99) + " рублей");
+        System.out.println(service.calculate(1000000, 24, 9.99) + " рублей");
 
         System.out.println();
         System.out.println("Кредит на 3 года. Ежемесячный платёж:");
-        System.out.println(service.calculate (1000000, 36, 9.99) + " рублей");
+        System.out.println(service.calculate(1000000, 36, 9.99) + " рублей");
 
     }
 
--- src/PaymentCalc.java
+++ src/PaymentCalc.java
@@ -1,16 +1,15 @@
 public class PaymentCalc {
     public int calculate(int amount, int months, double percent) {
-        double monthPercent = (percent / months) / 100;
+        double monthPercent = percent / (months * 100);
         double a = 1 + monthPercent;
         double b = months;
         double x = Math.pow(a, b);
-        double monthlyPayment = amount * ((monthPercent * x) / (x -1));
+        double monthlyPayment = amount * ((monthPercent * x) / (x - 1));
 
         int pay = (int) monthlyPayment;
 
         return pay;
     }
-
 
 
 }
